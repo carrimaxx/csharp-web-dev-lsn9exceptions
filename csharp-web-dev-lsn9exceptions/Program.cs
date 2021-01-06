b0cf5a1f4ca771044e90ed2d6da74508d45b6525
Maricar Walters <walterskai7@gmail.com>
2021-01-06 11:07:07 -0600

    Solved exercises on exceptions

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,19 +7,42 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+                throw new ArgumentOutOfRangeException("Cannot divide by zero");
+            else
+                return x / y;
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            int points = 0;
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentException("Invalid input, filename is either null or empty");
+            }
+            else if(fileName.EndsWith(".cs") == true)
+            {
+                points++;
+            }
+           
+            return points;
         }
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
-
+            try
+            {
+                double result = Program.Divide(7,0);
+                Console.WriteLine(result);
+                
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
@@ -27,7 +50,18 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> student in students)
+            {
+                try
+                {
+                    int test = Program.CheckFileExtension(student.Value);
+                    Console.WriteLine(student.Key + ": " + test);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(student.Key + ": {0}", e.Message);
+                }
+            }
         }
     }
 }
